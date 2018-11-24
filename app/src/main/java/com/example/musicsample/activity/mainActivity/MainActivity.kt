9b38bc83daf9cb8f45806acd01c3d84cd4070c9a
Jean-Christian Mbappe Besseme <jc.mbappe@gmail.com>
2018-11-24 11:19:43 +0100

    [MASTER] MV MainActivity

--- app/src/main/java/com/example/musicsample/activity/mainActivity/MainActivity.kt
+++ app/src/main/java/com/example/musicsample/activity/mainActivity/MainActivity.kt
@@ -1,16 +1,18 @@
-package com.example.musicsample.activity
+package com.example.musicsample.activity.mainActivity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.musicsample.R
 import com.example.musicsample.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(
+            this,
             R.layout.activity_main
         )
     }
