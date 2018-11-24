62840b3f3830bb0bafd2876c4bf063a3ff573123
Jean-Christian Mbappe Besseme <jc.mbappe@gmail.com>
2018-11-24 22:33:09 +0100

    [MASTER] country Iso

--- app/src/main/java/com/example/musicsample/repository/ItuneRepository.kt
+++ app/src/main/java/com/example/musicsample/repository/ItuneRepository.kt
@@ -5,15 +5,18 @@ import com.google.gson.Gson
 import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import java.util.Locale
 
 class ItuneRepository(okHttpClientClient: OkHttpClient, gson: Gson) :
     StreamingPlatformRepository(okHttpClientClient, gson) {
 
+    private val countryISO = Locale.getDefault().country.toLowerCase()
+
     override fun getTopHundredHotSongs(callback: NetworkCallback) {
         val url =
-            HttpUrl.parse("https://itunes.apple.com/fr/rss/topsongs/limit=100/json")?.newBuilder()
+            HttpUrl.parse("https://itunes.apple.com/$countryISO/rss/topsongs/limit=100/json")?.newBuilder()
                 ?.build().toString()
-
+        
         okHttpClient.newCall(
             Request.Builder()
                 .url(url)
