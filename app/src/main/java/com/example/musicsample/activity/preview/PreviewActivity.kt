1b7539862abe047aacebb124112c5ec394556111
Jean-Christian Mbappe Besseme <jc.mbappe@gmail.com>
2018-12-01 14:55:29 +0100

    [MASTER] PreviewActivity

---
+++ app/src/main/java/com/example/musicsample/activity/preview/PreviewActivity.kt
@@ -0,0 +1,12 @@
+package com.example.musicsample.activity.preview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PreviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preview)
+    }
+}
